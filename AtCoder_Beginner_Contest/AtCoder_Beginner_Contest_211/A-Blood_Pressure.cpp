5229c04db8ae09c547a1ed2adf03fdeb4528e488
unknown <shahariar.anu@northsouth.edu>
2021-11-03 23:55:33 +0600

    Beginner Contest 211 A-Blood Pressure

---
+++ AtCoder_Beginner_Contest/AtCoder_Beginner_Contest_211/A-Blood_Pressure.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<(float)(a-b)/3+b<<"\n";
+}
